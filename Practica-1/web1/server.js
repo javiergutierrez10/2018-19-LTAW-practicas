f47069a4e5dcedcc0aa63a37ad698f0d2783db09
Javier  Gutierrez De La Fuente <jguti@delta14.aulas.gsyc.urjc.es>
2019-02-04 12:35:51 +0100

    Actualizar

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -1,10 +1,17 @@
 var http = require('http');
-
-console.log("Arrancando servidor...")
-
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  console.log("Peticion atendida")
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
