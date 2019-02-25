3517ee99c25f04bf397c2b3f93df6d4bc3df7d76
Javier  Gutierrez De La Fuente <jguti@beta15.aulas.gsyc.urjc.es>
2019-02-25 12:16:11 +0100

    Actualizar

--- Practica-2/mi_tienda/mi_tienda/views.py
+++ Practica-2/mi_tienda/mi_tienda/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main.html', {'user': 'Obijuan'})
 
 def mi_funcion(request):
 	html = "Hola! Mi primera UrL!!"
